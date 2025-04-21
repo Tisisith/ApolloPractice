bf4953c79b4f10fbfdc59518744ee424b5bdf4f0
Nelson <nelsonespiritu21@gmail.com>
2025-04-21 08:29:03 +0800

    start exercise 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/pages/Home";
 import AboutMe from "./components/pages/AboutMe";
 import Contact from "./components/pages/Contact";
 import Flex from "./components/pages/Flexbox1";
+import Flex2 from "./components/pages/Flexbox2";
 
 const App = () => {
   return (
@@ -19,10 +20,14 @@ const App = () => {
         <Link to="/flexbox1">
           <h1>Exercise One</h1>
         </Link>
+        <Link to="/flexbox2">
+          <h1>Exercise Two</h1>
+        </Link>
       </nav>
 
       <Routes>
         <Route path="/flexbox1" element={<Flex />} />
+        <Route path="/flexbox2" element={<Flex2 />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/pages/Flexbox2.jsx
@@ -0,0 +1,7 @@
+import "../styles/ExerciseOne.css";
+
+const Flex2 = () => {
+  return <div></div>;
+};
+
+export default Flex2;
